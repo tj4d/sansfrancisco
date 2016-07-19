71f8452889561f96f847af7d60647cc2b0aac510
Robinson Greig <robinsongreig@gmail.com>
2016-07-19 12:55:43 -0400

    more color theme-ing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,10 +47,11 @@ $( document ).ready(function() {
   });
 
   /* 
-  ========= Announcement Background Color =========
+  ========= Changing Theme Color Color =========
   */
   var colors = ['#EDB0B1', '#F8DCB6', '#D9EDB0', '#B0EDB0', '#B0EDD7', '#B0DAED', '#B0B1ED', '#D4B0ED', '#EDB0E1'];
   var random_color = colors[Math.floor(Math.random() * colors.length)];
   $('.announcement').css('background-color', random_color);
-
+  $('.logo').css('text-shadow', '2px 2px' + random_color);
+  $('.active-nav').css('border-bottom', '5px solid' + random_color)
 });
