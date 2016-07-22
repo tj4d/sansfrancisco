4d8d40fdeca9850c37b8f1c9bc3f08469689071f
Robinson Greig <robinsongreig@gmail.com>
2016-07-22 00:58:23 -0400

    order jobs by date created at

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,7 +4,7 @@ class JobsController < ApplicationController
   # GET /jobs
   # GET /jobs.json
   def index
-    @jobs = Job.all
+    @jobs = Job.all.order(:created_at => :desc)
     @categories = {
       "Internship"=>"internship",
       "Apprenticeship"=>"apprenticeship",
