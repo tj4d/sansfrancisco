f250248462f0776b84c7db104171278ab99ca007
Robinson Greig <robinsongreig@gmail.com>
2016-08-20 13:52:19 -0400

    add signout link and default photo to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @resource_votes = @user.get_voted Resource
+    @resources = @resource_votes.all.order(:cached_votes_up => :desc)
     @publication_votes = @user.get_voted Publication
+    @publications = @publication_votes.all.order(:cached_votes_up => :desc)
+    @categories = {
+      "User Research"=>"user-research",
+      "Typography"=>"typography",
+      "Color Palette"=>"color-palette",
+      "Iconography"=>"iconography",
+      "Stock Photos"=>"stock-photos",
+      "Inspiration"=>"inspiration",
+      "Prototyping"=>"prototyping",
+      "Presentation"=>"presentation",
+      "Collaboration"=>"collaboration",
+    }
   end
 end
